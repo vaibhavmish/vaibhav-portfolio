a1321a95bab7d34944f121ddc9dfc76961bb01db
vaibhavmish <blacknodester@gmail.com>
2024-09-18 01:18:03 +0530

    hidden nav when scrolling down

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Navbar.css';
 import DarkModeToggle from './DarkModeToggle.js';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+const Navbar = () => {
+  const [isHidden, setIsHidden] = useState(false);  
+  const prevScrollY = useRef(0);  
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > prevScrollY.current) {
+        // User scrolled down more than the threshold
+         console.log("sadasd", currentScrollY, prevScrollY.current);
+        setIsHidden(true);
+      } else if(currentScrollY < prevScrollY.current){
+        // User scrolled up more than the threshold
+         console.log("else", currentScrollY, prevScrollY.current);
+        setIsHidden(false);
+      }
+
+      // Update the previous scroll position
+      prevScrollY.current = currentScrollY;
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the scroll listener when the component unmounts
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
     return(
-    <nav id="navbar">
+    <nav className={`navbar ${isHidden ? "hidden" : ""}`}>
        <ul className="nav-links"> 
           <li><Link to="/">HomePage</Link></li>
           <li><Link to="/Projects">Projects</Link></li>
