da49004c0c315e72fe3eaaf4718a073e521dcd9e
vaibhavmish <blacknodester@gmail.com>
2024-09-24 21:30:38 +0530

    added interactive resume in projects

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -3,7 +3,7 @@ const projectsData =[
 {id:1, title:'City-Skyline', description: 'The Skyline Project is a responsive design using creative CSS animations featuring a dynamic cityscape. The landscape transitions from night to day as the screen size reduces.', imageUrl: "./images/skyline-night.png" },
 {id:2, title:'Ferris Wheel', description: 'The project uses HTML, CSS, and JavaScript to visualize the rotation and movement of the Ferris wheel, with interactive features to control its speed and direction.', imageUrl:'./images/ferris-wheel.png' },
 {id:3, title:'Flappy Penguin', description: 'Description for project 1',  videoUrl:'./images/flappy-penguin.mp4' },
-{id:4, title:'Project 4', description: 'Description for project 1', imageUrl:'https://via.placeholder.com/150' },
+{id:4, title:'Interactive-Resume', description: 'An interactive resume built with React showcasing my skills, projects, and experience. Features responsive design, smooth transitions, and dynamic content for an engaging user experience.', imageUrl:'./images/interactive-resume-pic.png' },
 {id:5, title:'Project 5', description: 'Description for project 1', imageUrl:'https://via.placeholder.com/150' },
 {id:6, title:'Project 6', description: 'Description for project 1', imageUrl:'https://via.placeholder.com/150' },
 ]
